82e3711027c573cd7d47f123e6c6c945af1030d2
Ting Zhou <limtingzhou@gmail.com>
2018-12-29 17:26:07 +0800

    Modify express app to listen to heroku port

--- app.js
+++ app.js
@@ -61,6 +61,6 @@ app.get("/posts/:postTitle", function(req, res) {
 
 
 
-app.listen(3000, function() {
-  console.log("Server started on port 3000");
+app.listen(process.env.PORT || 3000, function() {
+  console.log(`Server started on port ${process.env.PORT || 3000}`);
 });
