e7f7e2c30a6fc87bcd3040d562ea4b7ae886fd91
Ting Zhou <limtingzhou@gmail.com>
2018-12-30 16:49:27 +0800

    Use MongoDB for persistent data
    Allows blog posts composed from /compose to be saved in a database.
    Includes node modules folder inside gitignore

--- app.js
+++ app.js
@@ -1,15 +1,13 @@
-//jshint esversion:6
-
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const _ = require("lodash");
+const mongoose = require("mongoose");
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
 const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
 
-const posts = [];
 
 const app = express();
 
@@ -18,8 +16,28 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
+
+
+
+mongoose.connect("mongodb+srv://admin-tingzhou:limtingzhou@cluster0-71gbh.mongodb.net/blogPosts?retryWrites=true", { useNewUrlParser: true} );
+const postSchema = new mongoose.Schema({
+  postTitle: {type: String, required: true},
+  postBody: {type: String, required: true}
+});
+
+const Post = mongoose.model("Post", postSchema);
+
+let day1 = new Post({postTitle:"Hello!", postBody: aboutContent});
+
+
+
+
+
 app.get("/", function(req, res) {
-  res.render("home", {startingContent: homeStartingContent, posts: posts});
+  Post.find({}, function(err, foundPosts) {
+    res.render("home", {startingContent: homeStartingContent, posts: foundPosts});
+  });
+
 
 });
 
@@ -36,24 +54,20 @@ app.get("/compose", function(req, res) {
 });
 
 app.post("/compose", function(req, res) {
-  let post = {
-    title: req.body.postTitle,
-    body: req.body.postBody,
-  };
-  posts.push(post);
+
+  let newPost = new Post({postTitle: req.body.postTitle, postBody: req.body.postBody});
+  newPost.save();
   res.redirect("/");
+
 });
 
-app.get("/posts/:postTitle", function(req, res) {
-  let requestedTitle = req.params.postTitle;
-  posts.forEach(function(post) {
-    let postTitle = _.lowerCase(post.title);
-    requestedTitle = _.lowerCase(requestedTitle);
-    if (postTitle === requestedTitle) {
-      console.log("match found!");
-      res.render("post", {postTitle: post.title, postBody: post.body});
-    }
+app.get("/posts/:postID", function(req, res) {
+
+  let requestedID = req.params.postID;
+  Post.findOne({_id: requestedID}, function(err, foundPost) {
+    res.render("post", {postTitle: foundPost.postTitle, postBody: foundPost.postBody});
   });
+
 });
 
 
